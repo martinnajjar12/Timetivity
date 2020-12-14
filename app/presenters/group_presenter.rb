f6e4183cbb634a6e9997800157705216c5423632
unknown <martin_najjar@yahoo.com>
2020-12-14 21:35:25 +0300

    Create a BasePresenter class

--- app/presenters/group_presenter.rb
+++ app/presenters/group_presenter.rb
@@ -1,16 +1,9 @@
-class GroupPresenter
-  def initialize(group, template)
-    @group = group
-    @template = template
-  end
-
-  def h
-    @template
-  end
+class GroupPresenter < BasePresenter
+  presents :group
 
   def icon
-    if @group.icon.attached?
-      h.image_tag(@group.icon, class: 'rounded-circle img-thumbnail icon')
+    if group.icon.attached?
+      h.image_tag(group.icon, class: 'rounded-circle img-thumbnail icon')
     else
       h.image_tag('default.png', class: 'rounded-circle img-thumbnail icon')
     end
