20c93ff29a8b149e00bd3281bc6bbbed6833969b
unknown <martin_najjar@yahoo.com>
2020-12-14 18:25:24 +0300

    Create a group controller and view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,11 +3,14 @@ class GroupsController < ApplicationController
 
   def index
     @groups = Group.all
-    # @user = User.find(params[:id])
   end
 
   def new
     @group = Group.new
+  end
+
+  def show
+    @group = Group.find(params[:id])
   end
 
   def create
