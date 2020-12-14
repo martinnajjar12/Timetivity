9c4ff9e2d54681efdb6361dc05bd7f81e0b8ab03
unknown <martin_najjar@yahoo.com>
2020-12-14 22:25:26 +0300

    Finish index page of groups

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -13,4 +13,20 @@ class BasePresenter
   def h
     @template
   end
+
+  # This method was made in to not use h.undefined method eah time in the presenter classes
+  def method_missing(*args, &block)
+    @template.send(*args, &block)
+  end
+
+  def user_avatar(user)
+    link_to user_path(user.id), class: 'link' do
+      if user.avatar.attached?
+        image_tag(user.avatar, class: 'rounded-circle')
+      else
+        gravatar_image_tag(user.email, class: 'rounded-circle')
+      end
+      content_tag(:h2, user.name, class: 'text-white')
+    end
+  end
 end
